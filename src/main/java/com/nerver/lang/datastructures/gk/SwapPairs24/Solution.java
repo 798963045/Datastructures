9ec0b01147ac28dac6f92db669ef0d8b55e5f7a8
penghao <penghao@sensetime.com>
2020-03-10 00:24:18 +0800

    swapPairs

--- src/main/java/com/nerver/lang/datastructures/gk/SwapPairs24/Solution.java
+++ src/main/java/com/nerver/lang/datastructures/gk/SwapPairs24/Solution.java
@@ -4,15 +4,51 @@ import com.nerver.lang.datastructures.gk.common.ListNode;
 
 public class Solution {
 
+    // 1->2->3->4
+
     public static ListNode swapPairs(ListNode head) {
         if(head == null || head.next ==null){
             return head;
         }
 
-        ListNode s1 = head;
-        ListNode s2 = head.next;
+        ListNode first = head;
+        ListNode sencond = head.next;
 
-        
+        ListNode p = swapPairs(sencond.next);
+        first.next = p;
+        sencond.next = first;
+        return sencond;
+
+    }
+
+    //
+    public static ListNode swapPairs2(ListNode head) {
+        // Dummy node acts as the prevNode for the head node
+        // of the list and hence stores pointer to the head node.
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode prevNode = dummy;
+
+        while ((head != null) && (head.next != null)) {
+
+            // Nodes to be swapped
+            ListNode firstNode = head;
+            ListNode secondNode = head.next;
+
+            // Swapping
+            prevNode.next = secondNode;
+            firstNode.next = secondNode.next;
+            secondNode.next = firstNode;
+
+            // Reinitializing the head and prevNode for next swap
+            prevNode = firstNode;
+            head = firstNode.next; // jump
+        }
+
+        // Return the new head node.
+        return dummy.next;
+
 
     }
 
@@ -25,7 +61,7 @@ public class Solution {
         h2.next = h3;
         h3.next = h4;
         h4.next = null;
-        swapPairs(h1);
+        ListNode result = swapPairs2(h1);
         System.out.println();
 
     }
