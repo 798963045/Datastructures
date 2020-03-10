09f8fee66596b5a1706766c6f8c4f15ff680556d
penghao <penghao@sensetime.com>
2020-03-10 17:05:54 +0800

    vaild parentheses

---
+++ src/main/java/com/nerver/lang/datastructures/gk/ValidParentheses/Solution2.java
@@ -0,0 +1,33 @@
+package com.nerver.lang.datastructures.gk.ValidParentheses;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Solution2 {
+
+    private HashMap<Character,Character> mappings;
+
+    public Solution2() {
+        this.mappings = new HashMap<>();
+        this.mappings.put(')','(');
+        this.mappings.put('}','{');
+        this.mappings.put(']','[');
+
+    }
+
+    public boolean isVaild(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i= 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(this.mappings.containsKey(c)){
+                char topElement = stack.empty()?'#':stack.pop();
+                if(topElement!=this.mappings.get(c)){
+                    return false;
+                }
+            }else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
