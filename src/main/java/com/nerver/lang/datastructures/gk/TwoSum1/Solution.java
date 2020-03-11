1fe897ffbaf51f7aef749f59a79a932e2e5bafc1
penghao <penghao@sensetime.com>
2020-03-11 15:51:18 +0800

    two sum 1

--- src/main/java/com/nerver/lang/datastructures/gk/TwoSum1/Solution.java
+++ src/main/java/com/nerver/lang/datastructures/gk/TwoSum1/Solution.java
@@ -18,11 +18,26 @@ public class Solution {
         return new int[]{};
     }
 
+
+
+    public int[] twoSum2(int[] nums,int target){
+        for(int i=0;i<nums.length-1;i++){
+            for (int j = i+1;j<nums.length;j++ ){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+
+
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
-        int target = 19;
+        int target = 18;
         Solution solution = new Solution();
-        int[] result = solution.twoSum(nums,target);
+        int[] result = solution.twoSum2(nums,target);
         System.out.println(result);
     }
 }
