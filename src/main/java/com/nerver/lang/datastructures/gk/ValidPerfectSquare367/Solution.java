f3153619b2cc3629ef78e3ec11067ffed44ad3d9
penghao <penghao@sensetime.com>
2020-03-26 22:18:34 +0800

    vaild perfect square 367

---
+++ src/main/java/com/nerver/lang/datastructures/gk/ValidPerfectSquare367/Solution.java
@@ -0,0 +1,47 @@
+package com.nerver.lang.datastructures.gk.ValidPerfectSquare367;
+
+public class Solution {
+
+
+    /**
+     * 牛顿迭代法
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare2(int num){
+        if(num<2) return true;
+        long x = num/2;
+        while (x*x>num){
+            x = (x+num/x)/2;
+        }
+        return (x*x==num);
+    }
+
+    /**
+     * 二分查找
+     * time compleixty ologn
+     * o1
+     * @param num
+     * @return
+     */
+    public boolean isPerfectSquare(int num){
+        if(num<2){
+            return true;
+        }
+        long left =2 ,right = num/2, x,guessSquared;
+        while (left<=right){
+            x = left+(right-left)/2;
+            guessSquared = x*x;
+            if(guessSquared==num){
+                return true;
+            }
+            if(guessSquared>num){
+                right = x-1;
+            }else {
+                left=x-1;
+            }
+        }
+        return false;
+
+    }
+}
