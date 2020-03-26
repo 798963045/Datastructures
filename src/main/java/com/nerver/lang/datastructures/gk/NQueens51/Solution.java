4cd781f37c5fa66432a5b984b95f8fe70d3c7c02
penghao <penghao@sensetime.com>
2020-03-26 11:46:31 +0800

    n queens 51

---
+++ src/main/java/com/nerver/lang/datastructures/gk/NQueens51/Solution.java
@@ -0,0 +1,13 @@
+package com.nerver.lang.datastructures.gk.NQueens51;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] temp = new int[3];
+        System.out.println(temp);
+    }
+
+}
